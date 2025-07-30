00e5b2ceb1a981e0ae493c14c5e3a4feee340feb
bhavyasri1010 <telubhavyasri@gmail.com>
2025-07-30 15:30:37 +0530

    Update App.py

--- App.py
+++ App.py
@@ -8,5 +8,5 @@ def hello():
     return "hello world"
 
 if __name__=="__main__":
-    port=int(os.envron.get("PORT" ,5000))
+    port=int(os.environ.get("PORT" ,5000))
     App.run(host="0.0.0.0" ,port=port)
